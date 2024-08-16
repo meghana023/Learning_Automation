93a05a07acaf1f88155bea540f6ee8c01c99a651
Meghana <meghana.023@gmail.com>
2024-08-16 15:46:03 +0530

    excel data updated

--- src/test/java/org/example/NseTestTata.java
+++ src/test/java/org/example/NseTestTata.java
@@ -13,8 +13,10 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class NseTestTata {
 
@@ -34,50 +36,82 @@ public class NseTestTata {
         Thread.sleep(500);
         // Open a website (optional)
         driver.get("https://www.screener.in/screens/566241/stock-list/");
-        WebElement element = driver.findElement(By.xpath("//a[contains(text(), 'Tata Elxsi')]"));
-        element.click();
 
-// Get the handle of the current window
-        String originalWindowHandle = driver.getWindowHandle();
+        String homeWindow = driver.getWindowHandle();
 
-        // Get all window handles
-        Set<String> windowHandles = driver.getWindowHandles();
-
-        // Iterate through window handles and switch to the new window
-        for (String handle : windowHandles) {
-            if (!handle.equals(originalWindowHandle)) {
-                driver.switchTo().window(handle);
-                break; // Exit loop once the new window is found and switched to
-            }
-        }
-
-        // Now you can interact with the new window
-        WebElement inverstor = driver.findElement(By.xpath("//a[contains(text(), 'Investors')]"));
-        inverstor.click();
-
-        // Do something with the browser (e.g., print the title of the page)
-        System.out.println("Page title is: " + driver.getTitle());
-
-       List<WebElement> rows = driver.findElements(By.xpath("//*[@id=\"quarterly-shp\"]/div/table")); // Example XPath
-        //List<WebElement> rows = driver.findElements(By.xpath("//*[@id=\"quarterly-shp\"]/div/table/tbody/tr[1]"));
-
-        // Create a new workbook and a sheet
         Workbook workbook = new XSSFWorkbook();
-        Sheet sheetb = workbook.createSheet("TataElxsi");
+        List<String> stocksList = Arrays.asList("Tata Elxsi", "Page Industries", "Solar Industries");
 
-        // Iterate over rows and write data to Excel
-        int rowIndex = 0;
-        for (WebElement rowElement : rows) {
-            Row row = sheetb.createRow(rowIndex++);
-            List<WebElement> cells = rowElement.findElements(By.tagName("td"));
+        for(String stock : stocksList){
+            WebElement element = driver.findElement(By.xpath("//a[contains(text(), '"+stock+"')]"));
+            element.click();
 
-            int cellIndex = 0;
-            for (WebElement cellElement : cells) {
-                Cell cell = row.createCell(cellIndex++);
-                cell.setCellValue(cellElement.getText());
+            // Get the handle of the current window
+            String originalWindowHandle = driver.getWindowHandle();
+
+            // Get all window handles
+            Set<String> windowHandles = driver.getWindowHandles();
+
+            // Iterate through window handles and switch to the new window
+            for (String handle : windowHandles) {
+                if (!handle.equals(originalWindowHandle)) {
+                    if(driver.switchTo().window(handle).getTitle().contains(stock)){
+                        driver.switchTo().window(handle);
+                        break; // Exit loop once the new window is found and switched to
+                    }
+                }
             }
-        }
 
+            // Now you can interact with the new window
+            WebElement inverstor = driver.findElement(By.xpath("//a[contains(text(), 'Investors')]"));
+            inverstor.click();
+
+            // Do something with the browser (e.g., print the title of the page)
+            System.out.println("Page title is: " + driver.getTitle());
+
+            WebElement rows = driver.findElement(By.id("quarterly-shp")); // Example XPath
+            //List<WebElement> rows = driver.findElements(By.xpath("//*[@id=\"quarterly-shp\"]/div/table/tbody/tr[1]"));
+
+            WebElement allColnHeader = rows.findElement(By.tagName("thead"));
+
+            // Now get all the TR elements from the table
+            List<WebElement> allRows = rows.findElements(By.tagName("tr"));
+
+            // Create a new workbook and a sheet
+
+            Sheet sheetb = workbook.createSheet(stock);
+
+            // Iterate over rows and write data to Excel
+            int rowIndex = 0;
+
+            List<WebElement> colnElements = allColnHeader.findElements(By.tagName("th"));
+            //for (WebElement colnElement : colnElements) {
+                Row rowHeader = sheetb.createRow(rowIndex++);
+                //List<WebElement> cells = colnElement.findElements(By.tagName("th"));
+
+                int cellIndex = 0;
+                for (WebElement cellElement : colnElements) {
+                    Cell cell = rowHeader.createCell(cellIndex++);
+                    cell.setCellValue(cellElement.getText());
+                }
+            //}
+
+            for (WebElement rowElement : allRows) {
+                Row row = sheetb.createRow(rowIndex++);
+                List<WebElement> cells = rowElement.findElements(By.tagName("td"));
+
+                cellIndex = 0;
+                for (WebElement cellElement : cells) {
+                    Cell cell = row.createCell(cellIndex++);
+                    cell.setCellValue(cellElement.getText());
+                }
+                if(cells.size()>0) {
+                    System.out.println(cells.get(cells.size() - 1).getText());
+                }
+            }
+            TimeUnit.SECONDS.sleep(5);
+            driver.switchTo().window(homeWindow);
+        }
 
         // Write the output to a file
         try (FileOutputStream fileOut = new FileOutputStream("webData.xlsx")) {
